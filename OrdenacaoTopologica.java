571c9c63ed3cb6ce48af22d8425c9320d05ddf78
lcazev <40309895+lcazev@users.noreply.github.com>
2019-12-01 19:40:30 -0300

    Update OrdenacaoTopologica.java

--- OrdenacaoTopologica.java
+++ OrdenacaoTopologica.java
@@ -2,186 +2,205 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class OrdenacaoTopologica
-{
-	private class Elo
-	{
-		/* IdentificaÁ„o do elemento. */
+public class OrdenacaoTopologica {
+	private class Elo {
+		/* Identifica√ß√£o do elemento. */
 		public int chave;
-		
-		/* N˙mero de predecessores. */
+
+		/* N√∫mero de predecessores. */
 		public int contador;
-		
-		/* Aponta para o prÛximo elo da lista. */
+
+		/* Aponta para o pr√≥ximo elo da lista. */
 		public Elo prox;
-		
+
 		/* Aponta para o primeiro elemento da lista de sucessores. */
 		public EloSuc listaSuc;
-		
-		public Elo()
-		{
+
+		public Elo() {
 			prox = null;
 			contador = 0;
 			listaSuc = null;
 		}
-		
-		public Elo(int chave, int contador, Elo prox, EloSuc listaSuc)
-		{
+
+		public Elo(int chave, int contador, Elo prox, EloSuc listaSuc) {
 			this.chave = chave;
 			this.contador = contador;
 			this.prox = prox;
 			this.listaSuc = listaSuc;
 		}
 	}
-	
-	private class EloSuc
-	{
-		/* Aponta para o elo que È sucessor. */
+
+	private class EloSuc {
+		/* Aponta para o elo que √© sucessor. */
 		public Elo id;
-		
-		/* Aponta para o prÛximo elemento. */
+
+		/* Aponta para o pr√≥ximo elemento. */
 		public EloSuc prox;
-		
-		public EloSuc()
-		{
+
+		public EloSuc() {
 			id = null;
 			prox = null;
 		}
-		
-		public EloSuc(Elo id, EloSuc prox)
-		{
+
+		public EloSuc(Elo id, EloSuc prox) {
 			this.id = id;
 			this.prox = prox;
 		}
 	}
 
-
-	/* Ponteiro (referÍncia) para primeiro elemento da lista. */
+	/* Ponteiro (refer√™ncia) para primeiro elemento da lista. */
 	private Elo prim;
-	/*Ponteiro (referÍncia) para ˙ltimo elemento da lista.*/
+	/* Ponteiro (refer√™ncia) para √∫ltimo elemento da lista. */
 	private Elo ult;
-	
-	/* N˙mero de elementos na lista. */
+
+	/* N√∫mero de elementos na lista. */
 	private int n;
-		
-	public OrdenacaoTopologica()
-	{
+
+	public OrdenacaoTopologica() {
 		prim = null;
 		n = 0;
 	}
-	
-	/* MÈtodo respons·vel pela leitura do arquivo de entrada. */
-	public void realizaLeitura(String nomeEntrada) throws IOException
-	{
+
+	/* M√©todo respons√°vel pela leitura do arquivo de entrada. */
+	public void realizaLeitura(String nomeEntrada) throws IOException {
 		FileReader arq = new FileReader(nomeEntrada);
 		BufferedReader lerArq = new BufferedReader(arq);
-		
+
 		String texto = lerArq.readLine();
-		
-		while(texto != null) {
-			
-			int x =  Integer.parseInt(texto.substring(0, 2).trim());
+
+		while (texto != null) {
+
+			int x = Integer.parseInt(texto.substring(0, 2).trim());
 			int y;
-			
-			if(texto.length() > 5)
-				 y = Integer.parseInt(texto.substring(4, 6).trim());
-			else 
+
+			if (texto.length() > 5)
+				y = Integer.parseInt(texto.substring(4, 6).trim());
+			else
 				y = Integer.parseInt(texto.substring(4, 5).trim());
-			
+
 			Elo elo_x = new Elo();
 			elo_x.chave = x;
-			
+
 			Elo elo_y = new Elo();
 			elo_y.chave = y;
-			
-			if(buscaElem(x) == false) {
-				if(prim == null) {
+
+			if (buscaElem(x) == false) {
+				if (prim == null) {
 					this.prim = elo_x;
 					this.ult = elo_x;
 				} else
 					this.ult.prox = elo_x;
-					ult = elo_x;
-				
+				ult = elo_x;
+
 				this.n++;
 			} else {
 				elo_x = retornaElemIgual(elo_x.chave);
 			}
-			
-			if(buscaElem(y) == false) {
+
+			if (buscaElem(y) == false) {
 				this.ult.prox = elo_y;
 				this.ult = elo_y;
-				
+
 				this.n++;
 			} else {
 				elo_y = retornaElemIgual(elo_y.chave);
 			}
-			
+
 			EloSuc eloSuc = new EloSuc();
 			eloSuc.id = elo_y;
 			eloSuc.prox = elo_x.listaSuc;
-			elo_x.listaSuc = eloSuc;			
-			
-			elo_y.contador++;	
-			
+			elo_x.listaSuc = eloSuc;
+
+			elo_y.contador++;
+
 			texto = lerArq.readLine();
 		}
-		
+
 		lerArq.close();
 	}
-	
+
 	public boolean buscaElem(int chave) {
-		if(prim == null)
+		if (prim == null)
 			return false;
-		
-		for(Elo i = this.prim; i != null; i = i.prox) {
-			if(i.chave == chave)
+
+		for (Elo i = this.prim; i != null; i = i.prox) {
+			if (i.chave == chave)
 				return true;
 		}
-		
-		return false;			
+
+		return false;
 	}
-	
-	/* MÈtodo para impress„o do estado atual da estrutura de dados. */
-	public void debug()
-	{
-		for(Elo i = this.prim; i != null; i = i.prox) {
+
+	/* M√©todo para impress√£o do estado atual da estrutura de dados. */
+	public void debug() {
+		for (Elo i = this.prim; i != null; i = i.prox) {
 			System.out.print(i.chave);
 			System.out.print(" predecessores: " + i.contador + ",");
 			System.out.print(" sucessores: ");
-			
-			if(i.listaSuc != null) {
+
+			if (i.listaSuc != null) {
 				EloSuc j = i.listaSuc;
-				
+
 				do {
 					System.out.print(j.id.chave + " -> ");
 					j = j.prox;
-					
-				} while(j != null);
-				
+
+				} while (j != null);
+
 				System.out.print("null");
 
 			} else {
 				System.out.print("null");
 			}
-						
+
 			System.out.println();
 		}
 	}
-	
-	/* MÈtodo respons·vel por executar o algoritmo. */
-	public boolean executa()
-	{
-		/* Preencher. */
-		
+
+	/* M√©todo respons√°vel por executar o algoritmo. */
+	public boolean executa() {
+		Elo p = prim;
+		prim = null;
+		while (p != null) {
+			Elo q = p;
+			p = q.prox;
+			if (q.contador == 0) {
+				q.prox = prim;
+				prim = q;
+			}
+		}
+		Elo i = prim;
+		while (i != null) {
+			System.out.println(i.chave);
+			this.n--;
+			prim = i.prox;
+			EloSuc j = i.listaSuc;
+			while (j != null) {
+				j.id.contador--;
+				if (j.id.contador == 0) {
+					j.id.prox = prim;
+					prim = j.id;
+				}
+				i.listaSuc = i.listaSuc.prox;
+				j = i.listaSuc;
+
+			}
+			i = prim;
+		}
+		if(n==0) {
+			return true;
+		} else {
+
 		return false;
+		}
 	}
-	
+
 	public Elo retornaElemIgual(int chave) {
-		for(Elo i = this.prim; i != null; i = i.prox) {
-			if(i.chave == chave)
+		for (Elo i = this.prim; i != null; i = i.prox) {
+			if (i.chave == chave)
 				return i;
 		}
-		
+
 		return null;
 	}
 }
